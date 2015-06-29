d078c301739236647464a00fd571cb871c07f904
shreyans264 <shreyans.264@gmail.com>
2015-06-29 21:22:44 +0530

    Update a.js

--- js files/a.js
+++ js files/a.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    var x, text
+    var x, text;
 
     // Get the value of the input field with id="numb"
-    x = document.getElementById("numb").value;
+    x = document.getElementById("numb").value
 
     // If x is Not a Number or less than one or greater than 10
     if (isNaN(x) || x < 1 || x > 10) {
