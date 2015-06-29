7ee77a76ca317c7936c49f5ab9fbb74ba71f68a7
shreyans264 <shreyans.264@gmail.com>
2015-06-29 21:26:32 +0530

    Update a.js

--- js files/a.js
+++ js files/a.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x, text
+    var x, text;
 
     // Get the value of the input field with id="numb"
     x = document.getElementById("numb").value;
@@ -8,7 +8,7 @@ function myFunction() {
     if (isNaN(x) || x < 1 || x > 10) {
         text = "Input not valid";
     } else {
-        text = "Input OK";
+        text = "Input OK"
     }
     document.getElementById("demo").innerHTML = text;
 }
